2ff61080d5980be041bef8dcd01995bbbec3524a
Ru <andy9729701@gmail.com>
2025-10-18 23:02:32 +0800

    updata

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,17 @@
-import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: "/",
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
+    base: "/",
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src')
+        }
+    },
+    server: {
+        port: 5173,
+        open: true
     }
-  },
-  server: {
-    port: 3000,
-    open: true
-  }
 })
